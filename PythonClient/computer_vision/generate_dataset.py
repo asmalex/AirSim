926004228cd6d1210823d25edcf00d47029eeb8a
Evan <emiller9045@gmail.com>
2021-10-09 17:28:47 -0400

    Data Labelling
    Added functionality to generate_dataset.py to create csv files that contain the coordinate of the center of the obstacle as well as rotation values for the change in the obstacle's orientation. Testing needed

--- PythonClient/computer_vision/generate_dataset.py
+++ PythonClient/computer_vision/generate_dataset.py
@@ -1,4 +1,4 @@
-# In settings.json first activate computer vision mode:
+# In settings.json first activate computer vision mode and set OriginGeoPoint to (0,0,2000):
 # https://github.com/Microsoft/AirSim/blob/master/docs/image_apis.md#computer-vision-mode
 
 import setup_path
@@ -10,6 +10,7 @@ import time
 import math
 import random
 from datetime import datetime
+import csv
 
 PRECISION_ANGLE = 4 # Fractions of a degree used in generating random pitch, roll, and yaw values
 PRECISION_METER = 100 # Fractions of a meter used in generating random distance values
@@ -56,6 +57,8 @@ while 1:
     r = random.randint(RADIUS_MIN * PRECISION_METER, RADIUS_MAX * PRECISION_METER) / PRECISION_METER
     phi = random.randint(0, 360 * PRECISION_ANGLE) / PRECISION_ANGLE
     theta = random.randint(0, 180 * PRECISION_ANGLE) / PRECISION_ANGLE
+    # Convert polar coordinates to cartesian for AirSim
+    pos = polarToCartesian(r, math.radians(theta), math.radians(phi))
 
     # Generate a random offset for the camera angle
     pitch = random.randint(0, VERT_FOV * PRECISION_ANGLE) / PRECISION_ANGLE - VERT_FOV / 2
@@ -64,7 +67,18 @@ while 1:
     roll = 0
     yaw = random.randint(0, FOV * PRECISION_ANGLE) / PRECISION_ANGLE - FOV/2
 
-    pos = polarToCartesian(r, math.radians(theta), math.radians(phi))
+    # Calculate coordinates of the center of the obstacle relative to the drone's new position and orientation
+    obs_r = r
+    obs_phi = -yaw
+    obs_theta = 90 + pitch
+    # Convert polar coordinates to cartesian for AirSim
+    obs_pos = polarToCartesian(obs_r, math.radians(obs_theta), math.radians(obs_phi))
+
+    # Record rotational transformation on obstacle for calculating coordinates of key locations relative to the center
+    obs_phi_offset = -phi
+    obs_theta_offset = 270 - theta
+
+    # Move the camera to our calculated position
     camera_pose = airsim.Pose(airsim.Vector3r(pos[0], pos[1], pos[2]), airsim.to_quaternion(math.radians(90 - theta + pitch), math.radians(roll), math.radians(phi + 180 + yaw))) #radians
     client.simSetVehiclePose(camera_pose, True)
 
@@ -77,6 +91,7 @@ while 1:
         #airsim.ImageRequest("0", airsim.ImageType.SurfaceNormals)
         ])
 
+    # Print image responses to files
     for i, response in enumerate(responses):
         filename = os.path.join(tmp_dir, str(e) + "_" + str(i))
         if response.pixels_as_float:
@@ -85,6 +100,11 @@ while 1:
         else:
             print("Type %d, size %d, pos %s" % (response.image_type, len(response.image_data_uint8), pprint.pformat(response.camera_position)))
             airsim.write_file(os.path.normpath(filename + '.png'), response.image_data_uint8)
+        # Create coordinate label file
+        with open(filename + '.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow([str(obs_pos[i]) for i in range(len(obs_pos))])
+            writer.writerow([obs_phi_offset, obs_theta_offset])
 
     pose = client.simGetVehiclePose()
     pp.pprint(pose)
