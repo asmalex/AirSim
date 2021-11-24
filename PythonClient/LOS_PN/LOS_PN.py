c25e7a34c5d9705674ba9198968747a2e94a31ee
Evan <emiller9045@gmail.com>
2021-11-24 12:11:30 -0500

    LOS PN Debugging
    Rearranged the obstacles in FlyingExampleMap to be linear in order to implement LOS PN. LOS_PN.py now compiles, but generates incorrect acceleration values.

--- PythonClient/LOS_PN/LOS_PN.py
+++ PythonClient/LOS_PN/LOS_PN.py
@@ -35,10 +35,11 @@ print("Setting up the drone for flight...")
 received = client.simGetObjectPose("Obstacle1")
 last_obs_pos = np.array([received.position.x_val, received.position.y_val, received.position.z_val]) # Global coordinates of the obstacle
 
+client.armDisarm(True)
 client.takeoffAsync().join()
 
 # Move the drone to a starting position nearby the first obstacle
-client.moveToPositionAsync(obs_pos[0]-10, obs_pos[1]-20, obs_pos[2]+1, 1).join()
+client.moveToPositionAsync(last_obs_pos[0]-50, last_obs_pos[1]-2, last_obs_pos[2]-1, 1).join()
 
 client.rotateToYawAsync(0, timeout_sec=3e+38, margin=2).join() # Rotate yaw to face forward
 
@@ -49,11 +50,11 @@ last_drone_or = [received.orientation.w_val, received.orientation.x_val, receive
 last_unit_LOS = normalize(last_obs_pos - last_drone_pos) # unit vector from the drone to the obstacle
 
 print("Beginning Proportional Navigation")
-print("Moving towrds obstacle 1...")
 i = 1
 t1 = time.time()
-time.sleep(0.01)
+client.moveByVelocityAsync(10, 0, 0, 5, drivetrain = 1).join() # Move slightly in the direction of the first obstacle to ensure that the initial closing velocity is positive
 while i < NUM_OBS + 1:
+    print("Moving towrds obstacle " + str(i) + "...")
     # Find the positions of the drone and the obstacle
     received = client.simGetObjectPose("Obstacle"+str(i))
     obs_pos = np.array([received.position.x_val, received.position.y_val, received.position.z_val]) # Global coordinates of the obstacle
@@ -74,11 +75,16 @@ while i < NUM_OBS + 1:
     ang_t = ang / (t2 - t1)
 
     # Calculate a unit vector in the direction of the desired LOS rotation
-    LOS_rot = unit_LOS - np.cross(np.inner(unit_LOS, last_unit_LOS / np.linalg.norm(unit_LOS)), last_unit_LOS / np.linalg.norm(unit_LOS))
+    LOS_rot = unit_LOS - np.inner(unit_LOS, last_unit_LOS / np.linalg.norm(unit_LOS) * last_unit_LOS / np.linalg.norm(unit_LOS))
     unit_LOS_rot = LOS_rot / np.linalg.norm(LOS_rot)
 
+    print(PG)
+    print(closing_velocity)
+    print(unit_LOS_rot)
+    print(ang_t)
+
     # Calculate the acceleration vector to be passed into the drone
-    LOS_accel = PG * np.cross(np.cross(closing_velocity, ang_t), unit_LOS_rot)
+    LOS_accel = PG * closing_velocity * unit_LOS_rot * ang_t
 
 
     #TODO: Accelerate the drone by the calculated vector
